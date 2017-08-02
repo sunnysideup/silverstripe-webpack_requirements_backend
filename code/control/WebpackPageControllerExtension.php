67112cc611286dbbae7cbd62b6f21aa5a312ed6f
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2017-08-02 14:01:08 +1200

    MAJOR: api change

--- code/control/WebpackPageControllerExtension.php
+++ code/control/WebpackPageControllerExtension.php
@@ -4,11 +4,42 @@
 class WebpackPageControllerExtension extends extension
 {
 
-    public function WebpackDevServer()
+    private static $port_for_webpack = 3000;
+
+    /**
+     *
+     * @return bool
+     */
+    public function IsNotWebpackDevServer()
+    {
+        return $this->IsWebpackDevServer() ? false : true;
+    }
+
+
+    /**
+     *
+     * @return bool
+     */
+    public function IsWebpackDevServer()
     {
         if (Director::isDev()) {
-            $socket = @fsockopen('localhost', 3000, $errno, $errstr, 1);
+            $socket = @fsockopen('localhost', $this->owner->Config()->get('port_for_webpack'), $errno, $errstr, 1);
             return !$socket ? false : true;
         }
+    }
+
+
+    /**
+     *
+     * @return string
+     */
+    public function WebpackBaseURL()
+    {
+        $str = Director::AbsoluteBaseURL('/');
+        if($this->IsWebpackDevServer()) {
+            $str = rtrim($str, '/') .':'.$this->owner->Config()->get('port_for_webpack').'/';
+        }
+
+        return $str;
     }
 }
