3174e4efc0ab68786c2567b4f10fce31eafb0cca
Nicolaas <github@sunnysideup.co.nz>
2017-11-17 11:06:15 +1300

    PATCH: fixes

--- _config.php
+++ _config.php
@@ -4,7 +4,7 @@ if(defined('webpack_requirements_backend_off')) {
     //do nothing
 } else {
     $options = Config::inst()->get('WebpackPageControllerExtension', 'webpack_enabled_themes');
-    if(count($options) === 0 || in_array(SSViewer::current_theme(), $options)) {
+    if(count($options) === 0 || in_array(Config::inst()->get('SSViewer', 'theme'), $options)) {
         Requirements::set_backend(new Requirements_Backend_For_Webpack());
     }
     unset($options);
