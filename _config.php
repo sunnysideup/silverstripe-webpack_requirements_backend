86b2b10eff21c0554fa0039089c71b9d978a1b11
Nicolaas <github@sunnysideup.co.nz>
2018-03-14 23:10:42 +1300

    PATCH: PSR-2

--- _config.php
+++ _config.php
@@ -1,10 +1,10 @@
 <?php
 
-if(defined('webpack_requirements_backend_off')) {
+if (defined('webpack_requirements_backend_off')) {
     //do nothing
 } else {
     $options = Config::inst()->get('WebpackPageControllerExtension', 'webpack_enabled_themes');
-    if(count($options) === 0 || in_array(Config::inst()->get('SSViewer', 'theme'), $options)) {
+    if (count($options) === 0 || in_array(Config::inst()->get('SSViewer', 'theme'), $options)) {
         Requirements::set_backend(new Requirements_Backend_For_Webpack());
     }
     unset($options);
