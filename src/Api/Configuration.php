69db3e15cef94def01a2bd7856cd22a01bec1fcb
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-09-03 10:22:20 +1200

    PATCH: bugfixes

---
+++ src/Api/Configuration.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace Sunnysideup\WebpackRequirementsBackend\Api;
+
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Manifest\ModuleResourceLoader;
+use SilverStripe\View\SSViewer;
+
+class Configuration
+{
+    use Configurable;
+
+    /**
+     * @var bool
+     */
+    private static $enabled = true;
+
+    /**
+     * you only need to set this if you have some themes that are enabled and some themes
+     * that do not run webpack
+     * @todo: implement
+     * @var array
+     */
+    private static $webpack_enabled_themes = [];
+
+    /**
+     * @var string
+     */
+    private static $webpack_theme = '';
+
+    /**
+     * this is the folder where the distilled files are placed.
+     * If your theme is foo then you will find the distilled files in themes/foo_dist
+     * @var string
+     */
+    private static $webpack_distribution_folder_extension = 'dist';
+
+    /**
+     * @var string
+     */
+    private static $webpack_src_folder_extension = 'src';
+
+    /**
+     * @var string
+     */
+    private static $webpack_node_modules_folder_extension = 'my_node_modules';
+
+    /**
+     * @var string
+     */
+    private static $webpack_raw_requirements_folder_extension = 'raw_requirements';
+
+    /**
+     * override webpack server for custom set ups
+     * set to true to make this class believe you are always running
+     * the webpack server
+     * @see IsWebpackDevServer
+     * @var bool
+     */
+    private static $is_webpack_server_override = false;
+
+    /**
+     * override webpack server for custom set ups
+     * this is the server used for checking if the webpack server is running
+     * @see IsWebpackDevServer
+     * @var string
+     */
+    private static $webpack_socket_server = 'localhost';
+
+    /**
+     * usually this is set to current domain
+     * only set if you need an alternative
+     * @see: WebpackBaseURL
+     * @var string
+     */
+    private static $webpack_server = '';
+
+    /**
+     * @var int
+     */
+    private static $webpack_port = 3000;
+
+    public static function get_theme_for_webpack(): string
+    {
+        $theme = (string) Config::inst()->get(self::class, 'webpack_theme');
+        if (! $theme) {
+            $array = SSViewer::get_themes();
+            if (! empty($array)) {
+                $theme = (string) array_shift($array);
+            }
+        }
+        if(! $theme) {
+            $theme = (string) Config::inst()->get(SSViewer::class, 'theme');
+        }
+        if(! $theme) {
+            $theme = 'please set theme ASAP';
+        }
+        return $theme;
+    }
+
+    /**
+     * @return string
+     */
+    public static function webpack_theme_folder(): string
+    {
+        return THEMES_DIR . '/' . self::get_theme_for_webpack();
+    }
+
+    /**
+     * @return bool
+     */
+    public function IsNotWebpackDevServer(): bool
+    {
+        return $this->IsWebpackDevServer() ? false : true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function IsWebpackDevServer(): bool
+    {
+        $override = $this->Config()->get('is_webpack_server_override');
+        if ($override) {
+            return $override;
+        }
+        if (Director::isDev()) {
+            $socket = @fsockopen(
+                $this->Config()->get('webpack_socket_server'),
+                $this->Config()->get('webpack_port'),
+                $errno,
+                $errstr,
+                1
+            );
+            return ! $socket ? false : true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string e.g. resources/themes/app_dist
+     */
+    public function WebpackFolderOnFrontEnd(): string
+    {
+        return ModuleResourceLoader::resourceURL($this->WebpackFolderOnFileSystem(false));
+    }
+
+    /**
+     * @param  boolean $withBase include baseFolder?
+     * @return string return /var/www/html/themes/app_dist
+     */
+    protected function WebpackFolderOnFileSystem(?bool $withBase = true): string
+    {
+        $location = '';
+        if ($withBase) {
+            $location .= Director::baseFolder() . '/';
+        }
+        $location .= THEMES_DIR . '/' . self::get_theme_for_webpack() . '/' . $this->Config()->get('webpack_distribution_folder_extension');
+
+        return $location;
+    }
+
+
+    public function getWebpackFile(string $file): string
+    {
+        foreach (['',  '.gz'] as $extension) {
+            $fileLocation = $this->WebpackFolderOnFileSystem(true) . '/' . $file . $extension;
+            if (file_exists($fileLocation)) {
+                $hash = filemtime($fileLocation);
+                return $this->WebpackFolderOnFrontEnd() . '/' . $file . '?x=' . $hash;
+            }
+        }
+
+        return '';
+    }
+
+    // /**
+    //  * @return string
+    //  */
+    // public function WebpackBaseURL(): string
+    // {
+    //     $webpackServer = $this->Config()->get('webpack_server');
+    //     if (! $webpackServer) {
+    //         $webpackServer = Director::AbsoluteBaseURL('/');
+    //     }
+    //     if ($this->IsWebpackDevServer()) {
+    //         $webpackServer = rtrim($webpackServer, '/') . ':' . $this->Config()->get('webpack_port');
+    //     }
+    //
+    //     return $webpackServer;
+    // }
+}
---
+++ src/Api/NoteRequiredFiles.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace Sunnysideup\WebpackRequirementsBackend\Api;
+
+use Exception;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SilverStripe\Assets\Filesystem;
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Flushable;
+use SilverStripe\View\SSViewer;
+
+class NoteRequiredFiles implements Flushable
+{
+    use Configurable;
+
+    /**
+     * @var bool
+     */
+    private static $save_requirements_in_folder = false;
+
+    /**
+     * @var string
+     */
+    private static $copy_css_to_folder = 'raw_requirements/css';
+
+    /**
+     * @var string
+     */
+    private static $copy_js_to_folder = 'raw_requirements/js';
+
+    public static function flush()
+    {
+        if (Director::isDev()) {
+            $theme = Configuration::get_theme_for_webpack();
+            if ($theme && self::can_save_requirements()) {
+                //make raw requirements writeable
+                $base = Director::baseFolder();
+                $themeFolderWithBase = $base . '/' . Configuration::webpack_theme_folder();
+                $srcFolder = $themeFolderWithBase . '/' . Config::inst()->get(Configuration::class, 'webpack_src_folder_extension');
+                $rawFolders = [
+                    $themeFolderWithBase,
+                    $themeFolderWithBase . '/' . Config::inst()->get(Configuration::class, 'webpack_distribution_folder_extension'),
+                    $themeFolderWithBase . '/' . Config::inst()->get(Configuration::class, 'webpack_node_modules_folder_extension'),
+                    $themeFolderWithBase . '/' . Config::inst()->get(Configuration::class, 'webpack_raw_requirements_folder_extension'),
+                    $srcFolder,
+                    $srcFolder . '/' . Config::inst()->get(self::class, 'copy_css_to_folder'),
+                    $srcFolder . '/' . Config::inst()->get(self::class, 'copy_js_to_folder'),
+                ];
+                foreach ($rawFolders as $folder) {
+                    Filesystem::makeFolder($folder);
+                }
+                $files = [
+                    $srcFolder . '/main.js',
+                    $srcFolder . '/style.scss',
+                ];
+                foreach ($files as $file) {
+                    if (! file_exists($file)) {
+                        @file_put_contents($file, '//add your customisations in this file');
+                    }
+                }
+
+                // $varArray = [
+                //     'themeName' => Configuration::get_theme_for_webpack(),
+                //     'devWebAddress' => isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : Director::protocolAndHost(),
+                //     'distributionFolder' => Configuration::get_theme_for_webpack().'_'.$this->Config()->get( 'webpack_distribution_folder_extension'),
+                // ];
+                // $str = 'module.exports = '.json_encode($varArray).'';
+                // @file_put_contents($base.'/'.$this->Config()->get('webpack_variables_file_location'), $str);
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public static function can_save_requirements(): bool
+    {
+        if (Director::isDev()) {
+            if (Config::inst()->get(self::class, 'save_requirements_in_folder')) {
+                if (RequirementsBackendForWebpack::is_themed_request()) {
+                    if (Configuration::get_theme_for_webpack()) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * @param  string $fileLocation
+     * @param  string $type
+     */
+    public function noteFileRequired(string $fileLocation, string $type = '')
+    {
+        if (! $type) {
+            $type = pathinfo($fileLocation, PATHINFO_EXTENSION);
+        }
+        $folderLocation = '';
+        switch ($type) {
+            case 'js':
+                $folderLocation = $this->Config()->get('copy_js_to_folder');
+                break;
+            case 'css':
+                $folderLocation = $this->Config()->get('copy_css_to_folder');
+                break;
+            default:
+                user_error('Please make sure to set type to js or css');
+                return;
+        }
+        $fileLocationArray = explode('?', $fileLocation);
+        $fileLocation = array_shift($fileLocationArray);
+        $base = Director::baseFolder();
+        $themeFolderWithBase = $base . '/' . Configuration::webpack_theme_folder();
+        $folderLocationWithBase = $themeFolderWithBase . '/' . $folderLocation;
+        Filesystem::makeFolder($folderLocationWithBase);
+        if (! file_exists($folderLocationWithBase)) {
+            user_error('Please update RequirementsBackendForWebpack for the right folder or create ' . $folderLocationWithBase);
+        }
+        if (strpos($fileLocation, '//') !== false) {
+            $logFile = $folderLocationWithBase . '/TO.INCLUDE.FROM.PAGE.SS.FILE.log';
+            $line = $_SERVER['REQUEST_URI'] . ' | ' . $fileLocation;
+            $this->addLinesToFile($logFile, $fileLocation);
+        } else {
+            $from = $fileLocation;
+            $line = '@import \'PROJECT_ROOT' . $from . '\'';
+            $logFile = $folderLocationWithBase . '/TO.INCLUDE.USING.WEBPACK.METHODS.log';
+            $this->addLinesToFile($logFile, $line);
+        }
+    }
+
+    protected function addLinesToFile($fileLocation, $line, $count = 0)
+    {
+        $line .= "\n";
+        $lines = '';
+        try {
+            $lines = [];
+            if (file_exists($fileLocation)) {
+                $lines = file($fileLocation);
+            }
+            if (! in_array($line, $lines, true)) {
+                //last catch!
+                if (is_writable($fileLocation)) {
+                    $handle = fopen($fileLocation, 'a');
+                    fwrite($handle, $line);
+                } else {
+                    echo '
+                    <br />
+                    Please run something like: <br />
+                    sudo touch ' . $fileLocation . ' && sudo chown www-data ' . $fileLocation . ' && sudo chmod 0775 ' . $fileLocation . '';
+                    user_error('
+                        Trying to write ' . $line . ' to ' . $fileLocation . '<br />
+                        ');
+                }
+            }
+        } catch (Exception $e) {
+            $this->makeFolderWritable($fileLocation);
+            $count++;
+            if ($count < 3) {
+                $this->addLinesToFile($fileLocation, $lines, $count);
+            }
+        }
+    }
+
+    protected function makeFolderWritable($fileLocation)
+    {
+        if (file_exists($fileLocation)) {
+            $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(dirname($fileLocation)));
+            foreach ($iterator as $item) {
+                chmod($item, '0664');
+            }
+        }
+    }
+}
